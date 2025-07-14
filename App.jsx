0cc95a98cc1c01eb049dfe7ff795ecc32db0af6b
DEVENDRA KUMAR SINGH <devendra3170@gmail.com>
2025-07-14 10:09:39 +0530

    Basic Setup for Quick Start ReactNative (JSX/JS) Version

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Text } from 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <Text>Hello Android</Text>
+  );
+}
